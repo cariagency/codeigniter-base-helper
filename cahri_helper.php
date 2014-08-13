75aa877734a7db3a69052ae30cdb19838c8df364
Julien Tessier <julien@cahri.com>
2014-08-13 16:58:44 +0400

    yes_no_ratio

--- cahri_helper.php
+++ cahri_helper.php
@@ -114,12 +114,9 @@
 	{
 		/*
 		 * Calculates a yes/no ratio where the biggest value return 1 and the lowest the cross-multiplication result
-		 * Example: 
-		 *		<div class="rating">
-		 *			<span class="yes" style="width:<?=round(yes_no_ratio($votes->yes, $votes->no)*100)?>%;">oui</span>
-		 *			<span class="no" style="width:<?=round(yes_no_ratio($votes->no, $votes->yes)*100)?>%;">non</span>
-		 *		</div>
-		 * Renders as: http://grab.by/zqv0
+		 * With $yes = 2 and $no = 1, render as :
+		 *      [==========]
+		 *      [=====]
 		 */
 
 		function yes_no_ratio($yes, $no)
